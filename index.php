c60b2dbaebf90b2f1f42311c356c2e699e9874c3
Rory van_Heerden <rory@eishgaming.co.za>
2015-08-21 00:49:54 +0200

    Close connection

--- index.php
+++ index.php
@@ -5,5 +5,6 @@ $redis->connect('127.0.0.1', 6379);
 $key = (rand(100, 999) * 0.001) * 1000;
 $redis->set($key, (rand(100, 999) * 0.001) * 1000);
 $value = $redis->get($key);
+$redis->close();
 
 echo $value;
