80c21e983b0550d8c9d82cae57dcf8fe0faa0296
Rory van_Heerden <rory@eishgaming.co.za>
2015-08-21 00:48:58 +0200

    Added code

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
+
+$key = (rand(100, 999) * 0.001) * 1000;
+$redis->set($key, (rand(100, 999) * 0.001) * 1000);
+$value = $redis->get($key);
+
+echo $value;
